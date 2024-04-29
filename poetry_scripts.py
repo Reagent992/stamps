4e2ddb382474c2792844ab6e79df968a0b43cfa8
Miron Sadykov <MironSadykov@yandex.ru>
2024-04-29 19:12:44 +0300

    feat: add iPython, add poetry poetry_scripts
    - iPython for comfy shell usage
    - Poetry don't expect "flat" projects like this,
    i have to do workaround(rename project in pyproject.toml)

---
+++ poetry_scripts.py
@@ -0,0 +1,37 @@
+import subprocess
+
+
+def dr():
+    """Запуск сервера разработки."""
+    cmd = ["python", "manage.py", "runserver"]
+    subprocess.run(cmd)
+
+
+def ds():
+    """Запуск Django Shell."""
+    cmd = ["python", "manage.py", "shell"]
+    subprocess.run(cmd)
+
+
+def cw():
+    """Запуск Celery worker."""
+    cmd = ["python", "-m", "celery", "-A", "config", "worker", "-l", "info"]
+    subprocess.run(cmd)
+
+
+def rb():
+    """Запуск контейнера с RabbitMQ."""
+    cmd = [
+        "docker",
+        "run",
+        "-it",
+        "--rm",
+        "--name",
+        "rabbitmq",
+        "-p",
+        "5672:5672",
+        "-p",
+        "15672:15672",
+        "rabbitmq:management",
+    ]
+    subprocess.run(cmd)
