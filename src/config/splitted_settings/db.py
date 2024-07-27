984d8450791c10a342d9ec7d844e65bba53acd3a
Miron Sadykov <MironSadykov@yandex.ru>
2024-07-27 18:36:57 +0300

    feat!: drop sqlite support

--- src/config/splitted_settings/db.py
+++ src/config/splitted_settings/db.py
@@ -1,32 +1,15 @@
-import sys
-from pathlib import Path
-
 from environs import Env
 
 env = Env()
 env.read_env()
 
-BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
-
-USE_POSTGRESQL = env.bool("USE_POSTGRESQL", default=False)
-
-if not USE_POSTGRESQL or "test" in sys.argv:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME", default="stamps"),
+        "USER": env.str("POSTGRES_USER", default="username"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD", default="unsafe-password123"),
+        "HOST": env.str("DB_HOST", default="db"),
+        "PORT": env.int("DB_PORT", default=5432),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": env.str("DB_NAME", default="PGDB"),
-            "USER": env.str("POSTGRES_USER", default="username"),
-            "PASSWORD": env.str(
-                "POSTGRES_PASSWORD", default="unsafe-password123"
-            ),
-            "HOST": env.str("DB_HOST", default="db"),
-            "PORT": env.int("DB_PORT", default=5432),
-        }
-    }
+}
